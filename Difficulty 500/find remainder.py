a0ff91ebaff2c9a5062c671dc63eb35f6d518a86
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-11-17 18:42:29 +0900

    Implement remainder finding program
    Added a program to find the remainder of two integers.

--- Difficulty 500/find remainder.py
+++ Difficulty 500/find remainder.py
@@ -1,3 +1,8 @@
+"""
+Find Remainder
+Write a program to find the remainder when an integer A is divided by an integer B.
+The first line contains an integer T, the total number of test cases. Then T lines follow, each line contains two Integers A and B.
+"""
 t=int(input())
 for i in range(t):
     a,b=map(int,input().split())
